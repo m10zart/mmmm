cd2091ee19a6c9508599bd582ed04c5538d40f8c
Joe Lee <perchl0rat1c@gmail.com>
2014-10-26 00:10:45 -0700

    before voting

---
+++ client/templates/notifications/notifications.js
@@ -0,0 +1,22 @@
+// ===== HELPERS =====
+Template.notifications.helpers({
+  notifications: function() {
+    return Notifications.find({userId: Meteor.userId(), read: false});
+  },
+  notificationCount: function() {
+    return Notifications.find({userId: Meteor.userId(), read: false}).count();
+  }
+})
+
+Template.notificationItem.helpers({
+  notificationPostPath: function() {
+    return Router.routes.postPage.path({_id: this.postId});
+  }
+});
+
+// ===== EVENTS =====
+Template.notifications.events({
+  'click a': function() {
+    Notifications.update(this._id, {$set: {read: true}});
+  }
+});
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,19 @@
-Meteor.publish('posts', function() {
-  return Posts.find();
+Meteor.publish('posts', function(sort, limit) {
+  check(sort, Object);
+  check(limit, Number);
+  return Posts.find({}, {sort: sort, limit: limit});
+});
+
+Meteor.publish('singlePost', function(id) {
+  check(id, String);
+  return Posts.find(id);
+})
+
+Meteor.publish('comments', function(postId) {
+  check(postId, String);
+  return Comments.find({postId: postId});
+});
+
+Meteor.publish('notifications', function() {
+  return Notifications.find({userId: this.userId, read: false});
 });
